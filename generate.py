ca4944dad3ebbb88eb78f3383abf4401bbd302a6
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2021-05-15 10:45:12 +0200

    Add files via upload

--- generate.py
+++ generate.py
@@ -8,6 +8,10 @@ file.close()
 #5-bottom left
 #6-bottom right
 #c-crossroads
+#7-approach1 crossing left right and bottom road
+#8-approach2 crossing left top and bottom road
+#9-approach3 crossing left right and top road
+#A-approach4 crossing right top and bottom road
 generated="<!DOCTYPE html>\n<html lang='en'>\n<head>\n<meta charset='UTF-8'>\n<meta http-equiv='X-UA-Compatible' content='IE=edge'>\n<link rel='stylesheet' href='style.css'>\n<title>Document</title>\n</head>\n<body>\n"
 for i in map:
     generated+="\n<div class='row' style='display: flex;'>"
@@ -28,6 +32,14 @@ for i in map:
             generated+="\n<div class='road6' style='display: inline-block;'>\n<div id='ver'></div>\n<div id='lsv'></div>\n<div id='rsv'></div>\n<div id='hor'></div>\n<div id='lsh'></div>\n<div id='rsh'></div>\n<div id='turn'></div>\n<div id='ls'></div>\n<div id='rs'></div>\n</div>"
         if j=='c':
             generated+="\n<div class='crossroads' style='display: inline-block;'>\n<div id='centre'></div>\n<div id='tc'></div>\n<div id='trs'></div>\n<div id='tls'></div>\n<div id='lc'></div>\n<div id='lrs'></div>\n<div id='lls'></div>\n<div id='bc'></div>\n<div id='brs'></div>\n<div id='bls'></div>\n<div id='rc'></div>\n<div id='rrs'></div>\n<div id='rls'></div>\n</div>"
+        if j=='7':
+            generated+="\n<div class='approach1' style='display: inline-block;'>\n<div id='s'></div>\n<div id='sts'></div>\n<div id='bsc'></div>\n<div id='bsls'></div>\n<div id='bsrs'></div>\n<div id='bls'></div>\n<div id='brs'></div>\n</div>"
+        if j=='8':
+            generated+="\n<div class='approach2' style='display: inline-block;'>\n<div id='s'></div>\n<div id='sts'></div>\n<div id='bsc'></div>\n<div id='bsls'></div>\n<div id='bsrs'></div>\n<div id='bls'></div>\n<div id='brs'></div>\n</div>"
+        if j=='9':
+            generated+="\n<div class='approach3' style='display: inline-block;'>\n<div id='s'></div>\n<div id='sts'></div>\n<div id='bsc'></div>\n<div id='bsls'></div>\n<div id='bsrs'></div>\n<div id='bls'></div>\n<div id='brs'></div>\n</div>"
+        if j=='A':
+            generated+="\n<div class='approach4' style='display: inline-block;'>\n<div id='s'></div>\n<div id='sts'></div>\n<div id='bsc'></div>\n<div id='bsls'></div>\n<div id='bsrs'></div>\n<div id='bls'></div>\n<div id='brs'></div>\n</div>"
     generated+="\n</div>"
 generated+="</body>\n</html>"
 file2 = open('index.html','w')
